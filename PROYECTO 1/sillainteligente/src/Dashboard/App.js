803c95cbc2a0e1c30a66b1076642b3380b88369c
Jose Wannan <2993999310101@ingenieria.usac.edu.gt>
2021-09-15 12:40:05 -0600

    changes

--- PROYECTO 1/sillainteligente/src/Dashboard/App.js
+++ PROYECTO 1/sillainteligente/src/Dashboard/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+
 import TiempoUso from '../Graficos/tiempo-uso/TiempoUso';
 import Horario from '../Graficos/horario/Horario';
 import Peso from '../Graficos/peso/Peso';
@@ -6,9 +6,11 @@ import Historial from '../Historial/Historial';
 import Movimiento from '../Graficos/movimiento/Movimiento';
 
 function App() {
+  
   return (
+    
     <div id="main-wrapper">
-          <header>
+          <header className="fades">
             <nav>
                <div className="menu-icon">
                   <i className="fa fa-bars fa-2x"></i>
@@ -18,52 +20,52 @@ function App() {
                </div>
             </nav>
          </header>
-      <div className="page-wrapper">
-        <div className="container-fluid">
-          <br />
-          <div className="row">
-            <div className="col-md-12 col-sm-12 col-lg-12">
-              <h1>INFORMACIÓN</h1>
-              <br />
-              <div className="card border-dark mb-3">
-                <div className="card-body">
-                    
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-md-12 col-sm-12 col-lg-12">
-              <h1>REPORTES DE USO</h1>
-              <br />
-              <div className="row">
-                <div className="col-md-6 col-sm-12 col-lg-6">
-                  <TiempoUso />
-                </div>
-                <div className="col-md-6 col-sm-12 col-lg-6">
-                  <Peso />    
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-md-6 col-sm-12 col-lg-6">
-                  <Movimiento />  
-                </div>
-                <div className="col-md-6 col-sm-12 col-lg-6">
-                  <Horario />
-                </div>
-              </div>
-              <div className="row">
-                  <div className="col-md-12 col-sm-12 col-lg-12">
-                      <Historial />
+         <div className="page-wrapper">
+          <div className="container-fluid">
+            <br />
+            <div className="row">
+              <div className="col-md-12 col-sm-12 col-lg-12">
+                <h1>INFORMACIÓN</h1>
+                <br />
+                <div className="card border-dark mb-3">
+                  <div className="card-body">
+                      
                   </div>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12 col-sm-12 col-lg-12">
+                <h1>REPORTES DE USO</h1>
+                <br />
+                <div className="row">
+                  <div className="col-md-6 col-sm-12 col-lg-6">
+                    <TiempoUso />
+                  </div>
+                  <div className="col-md-6 col-sm-12 col-lg-6">
+                    <Peso />    
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-md-6 col-sm-12 col-lg-6">
+                    <Movimiento />  
+                  </div>
+                  <div className="col-md-6 col-sm-12 col-lg-6">
+                    <Horario />
+                  </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-12 col-sm-12 col-lg-12">
+                        <Historial />
+                    </div>
+                </div>
               </div>
             </div>
           </div>
+          <footer className="footer">
+              © 2021 SMART CHAIR
+          </footer>
         </div>
-        <footer className="footer">
-            © 2021 SMART CHAIR
-        </footer>
-      </div>
     </div>
   );
 }
