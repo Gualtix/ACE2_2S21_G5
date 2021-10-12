1193f069895f1b9ec2eaa27eb226d6c0468ed539
Jose Wannan <2993999310101@ingenieria.usac.edu.gt>
2021-10-12 13:42:41 -0600

    [UPDATE] Endpoints - Informacion

--- PRACTICA 2/SERVER_APIREST/server.js
+++ PRACTICA 2/SERVER_APIREST/server.js
@@ -30,36 +30,212 @@ mongoClient.connect(urlMongo, { useUnifiedTopology: true })
         res.send('API ARQUI 2 :D');
     });
 
-    //USUARIO
-    app.post('/informacion',(req, res)=>{
-        const data = req.body;
+    //INFORMACION
+    app.get('/informacion/temperatura',(req, res)=>{
         res.header("Access-Control-Allow-Origin", "*");
-        if(data.Nombre != null || data.Ubicacion != null || data.Silla != null)
+        coleccion.find().sort({$natural:-1}).limit(1).toArray()
+        .then(result =>
         {
-            const informacion = {
-                "luminosidad": data.luminosidad,
-                "humedad": data.humedad,
-                "temperatura": data.temperatura,
-                "direccion": data.direccion,
-                "velocidad": data.velocidad
-            }
-            
-            coleccion.insertOne(informacion)
-            .then(result => {
-                console.log("Registro Insertado!");
-                res.status(200).send('Registro Insertado!');
-            })
-            .catch(error => {
-                console.error("Error al insertar un registro: ", error)
-                res.status(404).send('No se han insertado datos');
-            });
-        }
-        else{
-            res.status(404).send('No se han insertado datos');
-        }
+            console.log("Obtener datos!!");
+            res.status(200).send((result.temperatura));
+        })
+        .catch(error => {
+            res.status(404).send("error");
+            console.error(error)
+        });
     });
 
+    app.get('/informacion/humedad',(req, res)=>{
+        res.header("Access-Control-Allow-Origin", "*");
+        coleccion.find().sort({$natural:-1}).limit(1).toArray()
+        .then(result =>
+        {
+            console.log("Obtener datos!!");
+            res.status(200).send((result.humedad));
+        })
+        .catch(error => {
+            res.status(404).send("error");
+            console.error(error)
+        });
+    });
 
+    app.get('/informacion/velocidad',(req, res)=>{
+        res.header("Access-Control-Allow-Origin", "*");
+        coleccion.find().sort({$natural:-1}).limit(1).toArray()
+        .then(result =>
+        {
+            console.log("Obtener datos!!");
+            res.status(200).send((result.velocidad));
+        })
+        .catch(error => {
+            res.status(404).send("error");
+            console.error(error)
+        });
+    });
+
+    app.get('/informacion/direccion',(req, res)=>{
+        res.header("Access-Control-Allow-Origin", "*");
+        coleccion.find().sort({$natural:-1}).limit(1).toArray()
+        .then(result =>
+        {
+            console.log("Obtener datos!!");
+            res.status(200).send((result.direccion));
+        })
+        .catch(error => {
+            res.status(404).send("error");
+            console.error(error)
+        });
+    });
+
+    app.get('/informacion/luminosidad',(req, res)=>{
+        res.header("Access-Control-Allow-Origin", "*");
+        coleccion.find().sort({$natural:-1}).limit(1).toArray()
+        .then(result =>
+        {
+            console.log("Obtener datos!!");
+            res.status(200).send((result.luminosidad));
+        })
+        .catch(error => {
+            res.status(404).send("error");
+            console.error(error)
+        });
+    });
+
+    //INFORMACION JSON
+    app.get('/informacion/temperatura/json',(req, res)=>{
+        res.header("Access-Control-Allow-Origin", "*");
+        coleccion.find().sort({$natural:-1}).limit(1).toArray()
+        .then(result =>
+        {
+            console.log("Obtener datos!!");
+            let value = 
+            {
+                "temperatura": result.temperatura
+            }
+            res.status(200).json((value));
+        })
+        .catch(error => {
+            res.status(404).send("error");
+            console.error(error)
+        });
+    });
+
+    app.get('/informacion/humedad/json',(req, res)=>{
+        res.header("Access-Control-Allow-Origin", "*");
+        coleccion.find().sort({$natural:-1}).limit(1).toArray()
+        .then(result =>
+        {
+            console.log("Obtener datos!!");
+            let value = 
+            {
+                "humedad": result.humedad
+            }
+            res.status(200).json((value));
+        })
+        .catch(error => {
+            res.status(404).send("error");
+            console.error(error)
+        });
+    });
+
+    app.get('/informacion/velocidad/json',(req, res)=>{
+        res.header("Access-Control-Allow-Origin", "*");
+        coleccion.find().sort({$natural:-1}).limit(1).toArray()
+        .then(result =>
+        {
+            console.log("Obtener datos!!");
+            let value = 
+            {
+                "velocidad": result.velocidad
+            }
+            res.status(200).json((value));
+        })
+        .catch(error => {
+            res.status(404).send("error");
+            console.error(error)
+        });
+    });
+
+    app.get('/informacion/direccion/json',(req, res)=>{
+        res.header("Access-Control-Allow-Origin", "*");
+        coleccion.find().sort({$natural:-1}).limit(1).toArray()
+        .then(result =>
+        {
+            console.log("Obtener datos!!");
+            let value = 
+            {
+                "direccion": result.direccion
+            }
+            res.status(200).json((value));
+        })
+        .catch(error => {
+            res.status(404).send("error");
+            console.error(error)
+        });
+    });
+
+    app.get('/informacion/luminosidad/json',(req, res)=>{
+        res.header("Access-Control-Allow-Origin", "*");
+        coleccion.find().sort({$natural:-1}).limit(1).toArray()
+        .then(result =>
+        {
+            console.log("Obtener datos!!");
+            let value = 
+            {
+                "luminosidad": result.luminosidad
+            }
+            res.status(200).json((value));
+        })
+        .catch(error => {
+            res.status(404).send("error");
+            console.error(error)
+        });
+    });
+
+    //GENERAL
+    app.get('/informacion',(req, res)=>{
+        res.header("Access-Control-Allow-Origin", "*");
+        coleccion.find().sort({$natural:-1}).limit(1).toArray()
+        .then(result =>
+        {
+            console.log("Obtener datos!!");
+            let value = 
+            {
+                "porcentaje_humedad": result.humedad,
+                "celcius_temperatura": result.temperatura,
+                "kmh_viento": result.velocidad,
+                "direccion_viento": result.direccion,
+                "lumenes": result.luminosidad
+            }
+            res.status(200).json((value));
+        })
+        .catch(error => {
+            res.status(404).send("error");
+            console.error(error)
+        });
+    });
+
+    app.post('/informacion',(req, res)=>{
+        res.header("Access-Control-Allow-Origin", "*");
+        let result = req.body;
+        let value = 
+        {
+            "humedad": result.humedad,
+            "temperatura": result.temperatura,
+            "velocidad": result.velocidad,
+            "direccion": result.direccion,
+            "luminosidad": result.luminosidad
+        }
+        coleccion.insertOne(value)
+        .then(resultado => {
+            console.log("Registro Insertado!");
+            res.status(200).send('Registro Insertado!');
+        })
+        .catch(error => {
+            console.error("Error al insertar un registro: ", error)
+            res.status(404).send('No se han insertado datos');
+        });
+    });
 
     app.listen(port, () => {console.log(`Server corriendo en puerto ${port}!`) });
     
