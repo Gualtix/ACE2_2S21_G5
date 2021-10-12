1a74ea4df52366cce7b65741e95431082818a0ce
Gualtix <walkter@gmail.com>
2021-10-12 19:38:30 +0000

    wind

---
+++ PRACTICA 2/wapp/js/schema.js
@@ -0,0 +1,12 @@
+var snap = 
+{
+    "porcentaje_humedad":95,
+    "celcius_temperatura":75,      //Enviar Medida en Grados Celcius
+    "kmh_viento":75,               //Enviar Medida en Kilometros por Hora
+    "direccion_viento":"noreste",  //norte, nordeste,este,sudeste,sur,suroeste,oeste noreste
+    "lumenes": 75,  
+};
+
+
+
+
